766d1430ccf1b7f0829d0d5f11e770c7ceaf115b
AdrianYMNg <adrianng909@gmail.com>
2019-08-13 10:15:32 +0100

    Fixed issue: sql now pulls all the data properly

--- Load connection.R
+++ Load connection.R
@@ -8,7 +8,7 @@ con <- dbConnect(MySQL(),
 rs <- dbSendQuery(con, "SELECT * FROM iowa.train;")
 database <- fetch(rs , n = -1)
 
-#database[database==""]<-"NA"
+database[database=="0"] <- 1
 
 dbClearResult(rs)
 dbDisconnect(con)
